91ae836430c37f741e4be46d56bbb21b39b08301
freakin23 <rameezparwez@gmail.com>
2024-05-12 12:45:19 +0530

    added input output

--- a.cpp
+++ a.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 
+void io() {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a << " " << b << '\n';
+}
+
 int main() {
     std::cout << "Hello Git" << '\n';
+    io();
     return 0;
 }
