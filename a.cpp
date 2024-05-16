36ac59d63557f130f7b7181bea07db7cc8151f65
freakin23 <rameezparwez@gmail.com>
2024-05-16 11:53:23 +0530

    Added Even Odd Checker

--- a.cpp
+++ a.cpp
@@ -6,9 +6,15 @@ void io() {
     std::cout << a << " " << b << '\n';
 }
 
+bool evenOdd(int n) {
+    return (n & 1);
+}
+
 int main() {
     std::cout << "Hello Git" << '\n';
     io();
     
+    int n = 5;
+    std::cout << (evenOdd(n) ? "Odd" : "Even") << '\n';
     return 0;
 }
