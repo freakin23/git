b51c824f43d563460268419aa89ae084af8528a1
freakin23 <rameezparwez@gmail.com>
2024-05-16 11:57:12 +0530

    Added Palindrome Checker

--- a.cpp
+++ a.cpp
@@ -6,9 +6,20 @@ void io() {
     std::cout << a << " " << b << '\n';
 }
 
+bool palindrome(std::string st) {
+    int n = (int)st.size();
+    for (int i = 0; i < n / 2; i++) {
+        if (st[i] != st[n - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
     std::cout << "Hello Git" << '\n';
     io();
     
+    std::cout << (palindrome("racecar") ? "Is Palindrome" : "Not Palindrome") << '\n';
     return 0;
 }
