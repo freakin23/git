73ac096b281fa7ac1c8925885fc6fded3c4ab22b
freakin23 <rameezparwez@gmail.com>
2024-05-16 12:05:34 +0530

    solved error

--- a.cpp
+++ a.cpp
@@ -6,7 +6,6 @@ void io() {
     std::cout << a << " " << b << '\n';
 }
 
-<<<<<<< HEAD
 bool palindrome(std::string st) {
     int n = (int)st.size();
     for (int i = 0; i < n / 2; i++) {
@@ -15,21 +14,17 @@ bool palindrome(std::string st) {
         }
     }
     return true;
-=======
+}
 bool evenOdd(int n) {
     return (n & 1);
->>>>>>> branch2
 }
 
 int main() {
     std::cout << "Hello Git" << '\n';
     io();
     
-<<<<<<< HEAD
     std::cout << (palindrome("racecar") ? "Is Palindrome" : "Not Palindrome") << '\n';
-=======
     int n = 5;
     std::cout << (evenOdd(n) ? "Odd" : "Even") << '\n';
->>>>>>> branch2
     return 0;
 }
